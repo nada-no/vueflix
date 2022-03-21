7c331c90be3edb0a7adccfa194e667fc694c378d
iawadancuestamolina <1hiawcuestamolinaadan@gmail.com>
2022-03-21 16:39:52 +0100

    primer commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input v-model="email" placeholder="email">
+        <input v-model="password" placeholder="password">
+        <button @click="login">Login</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    created(){
+        this.$auth.$subscribe((mutation, state) => {
+            if (state.isAuthenticated) {
+                this.$router.push('/');
+            }
+        })
+    },
+    data() {
+        return {
+            email: 'user@user.com', password: 'useruser'
+            // email: '', password: ''
+        }
+    },
+    methods: {
+        async login() {
+            this.$auth.login(this.email, this.password);
+        }
+    }
+}
+</script>
