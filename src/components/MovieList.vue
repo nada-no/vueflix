c47f806e02bb7cc2b55bb036852a8ef4dc4a105f
iawadancuestamolina <1hiawcuestamolinaadan@gmail.com>
2022-03-22 20:24:01 +0100

    avanzando. Falta añadir variable movies al componente2

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,18 +1,19 @@
 <template>
-<div class="movie-list">
-        <div class="movie" v-for="movie in movies" :key="movie.id">
-            <router-link :to="`/movie/${movie.id}`">
-                <h3>{{ movie.title }}</h3>
-                <img :src="`https://apigerard.herokuapp.com/img/movies/thumbnailmk2/img${movie.id}.jpg`">
-            <h3>{{ movie.title }}</h3>
-            <img :src="`https://apigerard.herokuapp.com/img/movies/thumbnailmk2/img${movie.id}.jpg`">
-            </router-link>
-        </div>
-        </div>
-    
+  <div class="movie-list">
+    <div class="movie" v-for="movie in movies" :key="movie.id">
+      <router-link :to="`/movie/${movie.id}`">
+        <h3>{{ movie.title }}</h3>
+        <img
+          :src="`https://apigerard.herokuapp.com/img/movies/thumbnailmk2/img${movie.id}.jpg`"
+        />
+        <h3>{{ movie.title }}</h3>
+        <img
+          :src="`https://apigerard.herokuapp.com/img/movies/thumbnailmk2/img${movie.id}.jpg`"
+        />
+      </router-link>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
